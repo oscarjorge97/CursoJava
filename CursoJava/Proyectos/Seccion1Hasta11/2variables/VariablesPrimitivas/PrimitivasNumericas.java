782ccff35980927125cd0be642f0f3b9ef167b43
oscar_ytc <oscarjorge97@gmail.com>
2022-08-23 15:11:48 +0200

    actualizando

---
+++ CursoJava/Proyectos/Seccion1Hasta11/2variables/VariablesPrimitivas/PrimitivasNumericas.java
@@ -0,0 +1,11 @@
+package VariablesPrimitivas;
+
+public class PrimitivasNumericas {
+
+	public static void main(String[] args) {
+		byte numeroByte = 127;
+		System.out.println("numeroByte= "+numeroByte);
+
+	}
+
+}
